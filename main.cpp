a6f8fa30b4a2afe6f449a2a7d3b946e8068977f4
Marius.E <mariue16@uia.no>
2019-09-22 15:26:15 +0200

    Fixed an error in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main()
 {
 	std::cout << "Hello world" << std::endl;
-	std::cout << "A new line from the browser" << std:endl;
+	std::cout << "A new line from the browser" << std::endl;
+	//Don't add errors to my code
 
 }
